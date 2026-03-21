775d52bc62a2baca90b90bb0db3d00f17947cb75
gunner206 <gunner.alx@gmail.com>
2026-03-21 23:06:07 +0000

    Initial configuration

---
+++ school/faculty/urls.py
@@ -0,0 +1,9 @@
+#faculty/urls.py
+
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+]
